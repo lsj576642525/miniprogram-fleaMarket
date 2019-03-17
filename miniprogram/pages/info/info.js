b385240bd81e014b9fd425bc38d4cff8564f7109
linshijie <lin_shi_jie@126.com>
2019-03-17 17:07:31 +0800

    init

---
+++ miniprogram/pages/info/info.js
@@ -0,0 +1,80 @@
+// miniprogram/pages/info/info.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let db = wx.cloud.database()
+    console.log(db)
+    db.collection('info').where({
+      person: {
+        name: '小秘书'
+      }
+    }).get({
+      success(res) {
+      // 输出 [{ "title": "The Catcher in the Rye", ... }]
+        console.log(res.data[0])
+      }
+    })
+    wx.setNavigationBarTitle({
+      title: '消息'
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
